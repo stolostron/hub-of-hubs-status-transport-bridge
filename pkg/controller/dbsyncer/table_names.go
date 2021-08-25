8bb0f467856abedaed706b7b9e1da8ea2a98d793
golanshabi <golanshabi3@gmail.com>
2021-08-25 17:02:52 +0300

    Changed design.

--- pkg/controller/dbsyncer/table_names.go
+++ pkg/controller/dbsyncer/table_names.go
@@ -2,16 +2,16 @@ package dbsyncer
 
 const (
 	// ManagedClustersTableName table for managed clusters.
-	ManagedClustersTableName = "managed_clusters"
+	ManagedClustersTableName = "status.managed_clusters"
 	// ComplianceTableName table for policy compliance status.
-	ComplianceTableName = "compliance"
+	ComplianceTableName = "status.compliance"
 	// MinimalComplianceTableName table for minimal policy compliance status.
-	MinimalComplianceTableName = "aggregated_compliance"
+	MinimalComplianceTableName = "status.aggregated_compliance"
 
 	// LocalComplianceTableName table for policy compliance status.
-	LocalComplianceTableName = "local_compliance"
+	LocalComplianceTableName = "local_status.compliance"
 	// LocalPolicySpecTableName the name of the local policy spec table.
-	LocalPolicySpecTableName = "local_policies"
+	LocalPolicySpecTableName = "local_spec.policies"
 	// LocalPlacementRuleTableName the table for local Placement Rules.
-	LocalPlacementRuleTableName = "local_placementrules"
+	LocalPlacementRuleTableName = "local_spec.placementrules"
 )
