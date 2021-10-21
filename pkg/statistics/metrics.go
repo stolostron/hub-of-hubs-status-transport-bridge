ebe842245020197ad854f3ca04fdb189a1752ac4
olegsternberg <Oleg.Sternberg@ibm.com>
2021-10-21 13:30:39 +0300

    added transport statistics

--- pkg/statistics/metrics.go
+++ pkg/statistics/metrics.go
@@ -4,6 +4,7 @@ package statistics
 type bundleMetrics struct {
 	conflationUnit conflationUnitMeasurement // measures a time and conflations while bundle waits in CU's priority queue
 	database       timeMeasurement           // measures a time took by db worker to process bundle
+	totalReceived  int64                     // total received bundles of the specific type via transport
 }
 
 func newBundleMetrics() *bundleMetrics {
