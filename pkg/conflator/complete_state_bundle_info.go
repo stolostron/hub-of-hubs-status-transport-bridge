495795eec72a3d905066935c3d0152baea636cfd
maroon <maroon.ayoub@ibm.com>
2021-12-14 16:26:08 +0200

    refactored

--- pkg/conflator/complete_state_bundle_info.go
+++ pkg/conflator/complete_state_bundle_info.go
@@ -6,7 +6,7 @@ import (
 	"github.com/open-cluster-management/hub-of-hubs-status-transport-bridge/pkg/transport"
 )
 
-// newCompleteStateBundleInfo returns a new DeltaStateBundleInfo instance.
+// newCompleteStateBundleInfo returns a new completeStateBundleInfo instance.
 func newCompleteStateBundleInfo() bundleInfo {
 	return &completeStateBundleInfo{
 		bundle:   nil,
@@ -30,7 +30,7 @@ func (bi *completeStateBundleInfo) getMetadata() *BundleMetadata {
 	return bi.metadata
 }
 
-// updateBundle updates the wrapped bundle and metadata according to the sync mode.
+// updateBundle updates the wrapped bundle and metadata according to the complete-state sync mode.
 func (bi *completeStateBundleInfo) updateBundle(bundle bundle.Bundle) error {
 	bi.bundle = bundle
 
@@ -38,7 +38,7 @@ func (bi *completeStateBundleInfo) updateBundle(bundle bundle.Bundle) error {
 }
 
 // updateMetadata updates the wrapped metadata according to the complete-state sync mode.
-// createNewObjects boolean sets whether new metadata objects must be pointed to.
+// overwriteObject boolean sets whether to overwrite the current metadata or to create a new instance.
 func (bi *completeStateBundleInfo) updateMetadata(bundleType string, version *status.BundleVersion,
 	transportMetadata transport.BundleMetadata, overwriteObject bool) {
 	if !overwriteObject {
@@ -67,10 +67,11 @@ func (bi *completeStateBundleInfo) getTransportMetadataToCommit() transport.Bund
 
 // markAsProcessed releases the bundle content and marks transport metadata as processed.
 func (bi *completeStateBundleInfo) markAsProcessed(processedMetadata *BundleMetadata) {
-	if processedMetadata.bundleVersion.Equals(bi.metadata.bundleVersion) {
-		bi.bundle = nil
-		// if this is the same bundle that was processed then mark it as processed, otherwise leave
-		// the current (newer one) as pending.
-		bi.metadata.transportBundleMetadata.MarkAsProcessed()
+	if !processedMetadata.bundleVersion.Equals(bi.metadata.bundleVersion) {
+		return
 	}
+	// if this is the same bundle that was processed then mark it as processed, otherwise leave
+	// the current (newer one) as pending.
+	bi.bundle = nil
+	bi.metadata.transportBundleMetadata.MarkAsProcessed()
 }
